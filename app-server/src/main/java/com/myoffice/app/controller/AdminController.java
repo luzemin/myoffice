30decb6200bbd08b1866024b1ca3b18ce0f9bcc2
luzemin <592640424@qq.com>
2024-06-19 11:05:48 +0800

    try swagger @Operation

--- app-server/src/main/java/com/myoffice/app/controller/AdminController.java
+++ app-server/src/main/java/com/myoffice/app/controller/AdminController.java
@@ -3,9 +3,7 @@ package com.myoffice.app.controller;
 import com.myoffice.app.common.R;
 import com.myoffice.app.model.request.AdminRequest;
 import com.myoffice.app.service.AdminService;
-import com.myoffice.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +14,8 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @Autowired
-    private UserService userService;
-
     @PostMapping("/api/auth/admin/login")
     public R adminLogin(@RequestBody AdminRequest adminRequest) {
         return adminService.adminLogin(adminRequest);
-    }
-
-    @GetMapping("/api/admin/user/all")
-    public R getAllUsers() {
-        return userService.getAllUsers();
     }
 }
--- app-server/src/main/java/com/myoffice/app/controller/UserController.java
+++ app-server/src/main/java/com/myoffice/app/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.myoffice.app.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.log4j.Logger;
 import com.myoffice.app.common.R;
 import com.myoffice.app.model.request.UserRequest;
@@ -18,14 +19,21 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/api/auth/user/login")
+    @Operation(summary = "User login", description = "just pass the username and password")
     public R userLogin(@RequestBody UserRequest userRequest) {
         logger.info("user login with " + userRequest.getUsername());
         return userService.login(userRequest);
     }
 
     @PostMapping("/api/admin/user/create")
+    @Operation(summary = "Create user", description = "only admin user can create new user")
     public R createUser(@RequestBody UserRequest userRequest) {
         return userService.create(userRequest);
+    }
+
+    @GetMapping("/api/admin/user/all")
+    public R manageAllUsers() {
+        return userService.getAllUsers();
     }
 
     @GetMapping("/api/user/all")
